c310c20d1ca0b2d0dc59bb694b67a41b1ac8a418
Felix Kenton <robotfelix@gmail.com>
2018-07-09 10:31:28 +0100

    Refactor Rails 4.2 compatibility to reduce duplication (at the cost of more methods)

--- lib/active_record/postgres/constraints/schema_dumper.rb
+++ lib/active_record/postgres/constraints/schema_dumper.rb
@@ -8,34 +8,52 @@ module ActiveRecord
           # Rails >= 5.0
           def indexes_in_create(table, stream)
             super
-            constraints = @connection.constraints(table)
-            return unless constraints.any?
-            constraint_statements = constraints.map do |constraint|
-              name = constraint['conname']
-              conditions = constraint['consrc']
-              "    t.check_constraint :#{name}, #{conditions.inspect}"
-            end
+            constraints(table, stream)
+          end
+
+          private
+
+          def build_constraint_statement(_, name, conditions)
+            "    t.check_constraint :#{name}, #{conditions.inspect}"
+          end
+
+          def add_constraints_to_stream(stream, constraint_statements)
             stream.puts constraint_statements.sort.join("\n")
           end
         else
-          # Rails < 5.0 does not have hook for table creation so add separate statements
+          # Rails < 5.0 does not have hook for table creation so fall back to
+          # adding constraints after table creation.
           def indexes(table, stream)
             super
-            constraints = @connection.constraints(table)
-            return unless constraints.any?
-            constraint_statements = constraints.map do |constraint|
-              name = constraint['conname']
-              conditions = constraint['consrc']
-              statement_parts = [
-                  "add_check_constraint #{remove_prefix_and_suffix(table).inspect}",
-                  "#{name.inspect}",
-                  "#{conditions.inspect}"
-              ]
-              "  #{statement_parts.join(', ')}"
-            end
+            constraints(table, stream)
+          end
+
+          private
+
+          def build_constraint_statement(table, name, conditions)
+            statement_parts = [
+                "add_check_constraint #{remove_prefix_and_suffix(table).inspect}",
+                name.inspect,
+                conditions.inspect
+            ]
+            "  #{statement_parts.join(', ')}"
+          end
+
+          def add_constraints_to_stream(stream, constraint_statements)
             stream.puts constraint_statements.sort.join("\n")
             stream.puts
           end
+        end
+
+        def constraints(table, stream)
+          constraints = @connection.constraints(table)
+          return unless constraints.any?
+          constraint_statements = constraints.map do |constraint|
+            name = constraint['conname']
+            conditions = constraint['consrc']
+            build_constraint_statement(table, name, conditions)
+          end
+          add_constraints_to_stream(stream, constraint_statements)
         end
 
       end
